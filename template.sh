9ce4748d87c3127c2cb0da3ff7eb400d77c71308
Jmaraturn231 <yenidohjoel231101@gmail.com>
2025-08-20 16:15:51 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+## Create directories
+mkdir -p src
+mkdir -p research
+
+## Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trial.ipynb
+
+
+echo "Template structure created successfully."
